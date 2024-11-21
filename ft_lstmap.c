f82e0c2667ba92c1d4b6d1a4be54dfce52cf0c8e
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-11-21 17:04:43 +0100

    Update ft_lstmap.c
    Update no leaks

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: rwrobles <rwrobles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 14:59:06 by rwrobles          #+#    #+#             */
-/*   Updated: 2024/11/17 18:41:13 by rwrobles         ###   ########.fr       */
+/*   Updated: 2024/11/20 18:02:01 by rwrobles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,25 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_lst;
-	t_list	*new_elem;
+	t_list	*new_list;
+	t_list	*new_obj;
+	void	*temp;
 
-	if (!lst || !f)
+	if (!lst || !f || !del)
 		return (NULL);
-	new_lst = NULL;
+	new_list = NULL;
 	while (lst)
 	{
-		new_elem = ft_lstnew(f(lst->content));
-		if (!new_elem)
+		temp = f(lst->content);
+		new_obj = ft_lstnew(temp);
+		if (!new_obj)
 		{
-			ft_lstclear(&new_lst, del);
+			ft_lstclear(&new_list, del);
+			free (temp);
 			return (NULL);
 		}
-		ft_lstadd_back(&new_lst, new_elem);
+		ft_lstadd_back(&new_list, new_obj);
 		lst = lst->next;
 	}
-	return (new_lst);
+	return (new_list);
 }
